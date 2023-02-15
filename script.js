39ae0435a853665d4ba24229db3f42ba79b6c799
pradip patra <pradip.patra224@gmail.com>
2023-02-15 14:08:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function opaqueClick() {
+    document.getElementById('side-id').style.width = '0';
+    document.getElementById('opaque-id').style.display = 'none';
+}
+
+function barClick() {
+    document.getElementById('side-id').style.width = '230px';
+    document.getElementById('opaque-id').style.display = 'block';
+}
